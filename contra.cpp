7d6d015e8000f9c5c1b13beb3292f086e7f0e85e
VoNiRoNi <summer21_aleksey.vorobev2005@microsoftcloudhack.onmicrosoft.com>
2022-11-28 22:52:56 +0300

    V 1.0.1

--- contra.cpp
+++ contra.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <cctype>   // для функции isdigit
+#include <iostream> // это база
+#include <string> // это строчки
+#include <algorithm> //это ТОП
+#include <vector> // это друг
+#include <map> // это для души
+#include <cctype>   // для функции isdigit которая не работает :(((
 #include <cstdlib>  // для функции atoi
 
 #define ln "\n"
@@ -15,49 +15,101 @@ using namespace std;
 
 map<string,string> m;
 
-bool is_digits(string str)
+bool is_digits(string str) // Проверка является ли строка числом
 {
     return str.find_first_not_of("0123456789") == string::npos;
 }
 
+vector<string> to_string_vector(char **a, int n) // перевод в векто строк
+{
+    vector<string> v(n);
+    for(int i = 0; i < n; i++)
+    {
+        v[i] = a[i];
+    }
+    re v;
+}
+
+
+vector<string> arguments = {"-o", "-help", "-version", "-n"}; // Все доступныые аргументы
+
+
+vector<string> get_format_and_name(string str) // получени названия и расшерения файла
+{
+    string name = str.substr(0,str.find_last_of('.'));
+    string format = str.substr(str.find_last_of('.'),str.size() - str.find_last_of('.'));
+    vector<string> a(2);
+    a[0] = name;
+    a[1] = format;
+    re a;
+}
+
 int main(int argc, char **argv)
 {
-    ll n = 2;
+    ll n = 2;// дефолтное значение
     ll ans = 0;
     string f = "a.txt";
-    for(int i = 1; i < argc; i++)
-    {
-        string s = argv[i];
-        if(s == "-o")
+    
+    vector<string> args = to_string_vector(argv, argc); // перевод в вектор
+
+    for(int i = 1; i < argc; i++)// цикл по всем аргументам
+    {   
+        if(args[i] == "-o") // обработка аргумента -о
         {
-            if(i+1 >= argc)
+            if(i+1 >= argc || args[i+1][0] == '-') // проверка  задано ли значение аргумента или идут ли два аргумента подряд 
             {
-                cout << "Net znachenia argumenta" << s << ln;
+                cout << "[Warnning] Net znachenia argumenta: " << "<" << args[i] << ">" << ln;
+                continue;
             }
             else
             {
-                f = argv[i+1];
-                i++;
+                f = args[i+1];
+                if(f.find_last_of('.') == string::npos)// проверка на то что задано расшерение файла
+                {
+                    cout << "[Warnning] Ne zadan format txt dla fayla! Otet budet soxranen v fayle : " << f << ".txt" << ln;
+                    f+=".txt"; //добавляем расшерение txt к файлу
+                    freopen(f.c_str(), "w",stdout);
+                    i++;
+                }
+                else
+                {
+                    vector<string> file = get_format_and_name(f);
+                    if(file[1] != ".txt")// Проверка расшерения файла
+                    {
+                        cout << "[Warnning] Neverniy format fayla! Otet budet soxranen v fayle : " << file[0] << ".txt" << ln;
+                        f = file[0] + ".txt";
+                        freopen(f.c_str(), "w",stdout);
+                        i++;
+                    }
+                }
             }
         }
-        if(s == "-help")
+        
+        
+        if(args[i] == "-help")
         {
-            cout << "Vse dostupnye comandy:\n -version : versiya programmy\n -n : peredaty colichestvo chisel dly clogenia\n -o <file name> : fail gde bydet soxranen rezultat" << ln;
+            cout << "Vse dostupnye comandy:\n -version : versiya programmy\n -n : peredaty colichestvo chisel dly clogenia\n -o <file name.txt> : fail gde bydet soxranen rezultat" << ln;
+            re 0;
         }
-        if(s == "-version")                                                                                                                                                     
+        
+
+        if(args[i] == "-version")                                                                                                                                                     
         {
-            cout << "V 1.0.0";
-            re 1;                                                                                                         
+            cout << "V 1.0.1" << ln;
+            re 0;                                                                                                         
         }
-        if(s == "-n")
+        
+
+        if(args[i] == "-n")
         {
-            if(i+1 >= argc)
+            if(i+1 >= argc || args[i+1][0] == '-')
             {
-                cout << "Net znachenia argumenta" << ln;
+                cout << "[Warnning] Net znachenia argumenta: " << "<" << args[i] << ">" << ln;
+                continue;
             }
             else
             {
-                string l = argv[i+1];
+                string l = args[i+1];
                 if(is_digits(l))
                 {
                     n = stoi(l);
@@ -65,18 +117,26 @@ int main(int argc, char **argv)
                 }
                 else
                 {
-                    cout << "Ne Natyralnoe chislo";
+                    cout << "[Warning] Ne Natyralnoe chislo" << ln;
+                    i++;
                 }
             }
         }
-        freopen(f.c_str(), "w",stdout);
-        for(ll i = 0; i < n;i++)
+
+        if(args[i][0] == '-'  && find(arguments.begin(), arguments.end(),args[i]) == arguments.end())
         {
-            ll x;
-            cin >> x;
-            ans+=x;
+            cout << "Neponytniy argument : <" << args[i] << ">" << ln;
         }
-        cout << ans;
-        re 0;
     }   
+    
+    
+    for(ll i = 0; i < n;i++)
+    {
+           ll x;
+           cin >> x;
+           ans+=x;
+    }
+    
+    cout << ans;
+    re 0;
 }
