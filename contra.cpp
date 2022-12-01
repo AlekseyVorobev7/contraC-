3b11249eda46a013656f87b501e4de82964754be
VoNiRoNi <summer21_aleksey.vorobev2005@microsoftcloudhack.onmicrosoft.com>
2022-12-01 18:22:08 +0300

    V 1.0.2

--- contra.cpp
+++ contra.cpp
@@ -13,14 +13,14 @@
 
 using namespace std;
 
-map<string,string> m;
+//map<string,string> m;
 
 bool is_digits(string str) // Проверка является ли строка числом
 {
     return str.find_first_not_of("0123456789") == string::npos;
 }
 
-vector<string> to_string_vector(char **a, int n) // перевод в векто строк
+vector<string> to_string_vector(char **a, int n) // перевод в вектор строк
 {
     vector<string> v(n);
     for(int i = 0; i < n; i++)
@@ -86,23 +86,23 @@ int main(int argc, char **argv)
         }
         
         
-        if(args[i] == "-help")
+        if(args[i] == "-help")// все доступные команды
         {
             cout << "Vse dostupnye comandy:\n -version : versiya programmy\n -n : peredaty colichestvo chisel dly clogenia\n -o <file name.txt> : fail gde bydet soxranen rezultat" << ln;
             re 0;
         }
         
 
-        if(args[i] == "-version")                                                                                                                                                     
+        if(args[i] == "-version")// версия                                                                                                                                               
         {
             cout << "V 1.0.1" << ln;
             re 0;                                                                                                         
         }
         
 
-        if(args[i] == "-n")
+        if(args[i] == "-n")// проверка на заданое количество чисел
         {
-            if(i+1 >= argc || args[i+1][0] == '-')
+            if(i+1 >= argc || args[i+1][0] == '-') // есть ли значение аргумента
             {
                 cout << "[Warnning] Net znachenia argumenta: " << "<" << args[i] << ">" << ln;
                 continue;
@@ -110,7 +110,7 @@ int main(int argc, char **argv)
             else
             {
                 string l = args[i+1];
-                if(is_digits(l))
+                if(is_digits(l)) // проверка является ли числом
                 {
                     n = stoi(l);
                     i++;
@@ -123,13 +123,14 @@ int main(int argc, char **argv)
             }
         }
 
-        if(args[i][0] == '-'  && find(arguments.begin(), arguments.end(),args[i]) == arguments.end())
+        if(args[i][0] == '-'  && find(arguments.begin(), arguments.end(),args[i]) == arguments.end()) // обработка не известных аргументов
         {
             cout << "Neponytniy argument : <" << args[i] << ">" << ln;
         }
     }   
+
     
-    
+
     for(ll i = 0; i < n;i++)
     {
            ll x;
@@ -137,6 +138,6 @@ int main(int argc, char **argv)
            ans+=x;
     }
     
-    cout << ans;
+    cout << "Suma zadanyx " << n << " chisel = " << ans;
     re 0;
 }
